2083c4690be4e7d92c4b01eaf09829d651460740
AdiDahari <dahariadi16@gmail.com>
2021-03-14 12:08:04 +0200

    Part 2 Changes

--- Demo.cpp
+++ Demo.cpp
@@ -1,37 +1,49 @@
-/**
- * Demo program for snowman exercise.
- * 
- * Author: Erel Segal-Halevi
- * Since : 2021-02
- */
-
 #include "snowman.hpp"
-
+#include <string>
 #include <iostream>
-#include <stdexcept>
 using namespace std;
 
 int main()
 {
-	cout << ariel::snowman(11114411) << endl; /* Should print:
-_===_
-(.,.)
-( : )
-( : )
-	*/
-	cout << ariel::snowman(33232124) << endl; /* Should print:
+    cout << ariel::snowman(11111111) << endl; /*Should Print:
+ _===_
+ (.,.) 
+<( : )>
+ ( : ) */
+    cout << ariel::snowman(22222222) << endl; /*Should Print:
+  ___ 
+ .....
+\(o.o)/
+ (] [) 
+ (" ") */
+    cout << ariel::snowman(33333333) << endl; /*Should Print:
+   _  
+  /_\ 
+ (O_O) 
+/(> <)\
+ (___) */
+    cout << ariel::snowman(44444444) << endl; /*Should Print:
+  ___ 
+ (_*_)
+ (- -) 
+ (   ) 
+ (   ) */
+    cout << ariel::snowman(32143121) << endl; /*Should Print:
+   _  
+  /_\ 
+ (..-) 
+/(] [)>
+ ( : ) */
+    cout << ariel::snowman(33232124) << endl; /*Should Print:
    _
   /_\
 \(o_O)
  (] [)>
- (   )
-	*/
-	try
-	{
-		cout << ariel::snowman(5) << endl; // Exception - not a valid code
-	}
-	catch (exception &ex)
-	{
-		cout << "   caught exception: " << ex.what() << endl; // should print "Invalid code '5'"
-	}
+ (   ) */
+    cout << ariel::snowman(32221133) << endl; /*Should Print:
+   _  
+  /_\ 
+ (o.o) 
+<(> <)>
+ (___) */
 }
